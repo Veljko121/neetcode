37d5cb749d134fbf05cf12ec0ffd0e6b5d5b2cb6
Veljko Nikolić <nikolicveljko01@gmail.com>
2026-04-13 22:38:14 +0200

    Arrays & Hashing: Group Anagrams - Python

---
+++ problems/arrays-and-hashing/6-group-anagrams/python/app.py
@@ -0,0 +1,33 @@
+from typing import List
+from collections import defaultdict
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    res = defaultdict(list)
+
+    numOfChars = ord("z") - ord("a") + 1
+    for s in strs:
+        count = [0] * numOfChars
+
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+
+        res[tuple(count)].append(s)
+    
+    return list(res.values())
+
+def main():
+    strs1 = ["act", "pots", "tops", "cat", "stop", "hat"]
+    out1 = [["act","cat"],["pots","tops","stop"],["hat"]]
+    strs2 = ["x"]
+    out2 = [["x"]]
+
+    res1 = groupAnagrams(strs1)
+    res2 = groupAnagrams(strs2)
+
+    if res1 != out1 or res2 != out2:
+        print("Actual results not equal with expected results.")
+    else:
+        print("Results correct.")
+
+if __name__ == "__main__":
+    main()
